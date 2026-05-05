d165e8ab8e2d544bb908860fc6c49a195966a6ce
Lis <genia.grishenko@gmail.com>
2026-05-05 12:50:04 +0300

    SearchBar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,1 +1,16 @@
 // import { useState } from "react";
+// import css from "./App.module.css";
+import SearchBar from "../SearchBar/SearchBar";
+import { Toaster } from "react-hot-toast";
+
+export default function App() {
+  const handleSearch = (searchTerm: string) => {
+    console.log("Шукаємо:", searchTerm);
+  };
+  return (
+    <>
+      <Toaster />
+      <SearchBar onSubmit={handleSearch} />
+    </>
+  );
+}
